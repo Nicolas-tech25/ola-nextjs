f347143cf7e6d8a12bf80a51995ce5d4f8a79c31
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-01-16 11:49:02 -0300

    paginas iniciais

---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+export default function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href=""></a>Home
+        </li>
+        <li>
+          <a href=""></a>Equipe
+        </li>
+        <li>
+          <a href="">Serviços</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
