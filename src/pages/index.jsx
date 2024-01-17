56c5b44789ed1cfa8c74b91fa2444cf0c0b5d5be
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-01-17 09:33:09 -0300

    ccd

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,14 @@
 import Menu from "@/components/Menu";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Meu app Next.js</title>
+        <meta name="description" content="Primeira página..."></meta>
       </Head>
+
       <h1>Olá next.js!</h1>
       <Menu />
 
--- src/pages/servicos.jsx
+++ src/pages/servicos.jsx
@@ -1,8 +1,13 @@
 import Menu from "@/components/Menu";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Meu app Next.js</title>
+        <meta name="description" content="Página serviços..."></meta>
+      </Head>
       <h1>Olá next.js!</h1>
       <Menu />
 
